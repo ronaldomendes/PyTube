b7cebe3f8b414665400b60e360a56c435a7ef3f8
Ronaldo Mendes <mendes-ronaldo@live.com>
2023-10-25 20:25:06 -0300

    Adding dynamic output path for Windows

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from pytube import YouTube
 
 url = 'https://www.youtube.com/watch?v=U0kCRbSyjmo'
@@ -6,7 +8,6 @@ url = 'https://www.youtube.com/watch?v=U0kCRbSyjmo'
 def on_download_progress(stream, chunk, bytes_remaining):
     bytes_downloaded = stream.filesize - bytes_remaining
     percentage = bytes_downloaded * 100 / stream.filesize
-
     print(f'Downloading... {int(percentage)}%')
 
 
@@ -15,5 +16,6 @@ yt.register_on_progress_callback(on_download_progress)
 video = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 
 print("Loading...")
-
-video.download()
+main_path = os.path.expanduser(os.getenv('USERPROFILE')).replace('\\', '/')
+user_path = f"{main_path}/Downloads/{yt.author}"
+video.download(output_path=user_path)
